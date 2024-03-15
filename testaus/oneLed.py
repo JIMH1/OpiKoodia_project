a195f12145856f9a8649a8acc1a9ec4e1b5c9691
EmilHellsten <98455448+EmilHellsten@users.noreply.github.com>
2024-03-15 19:15:56 +0200

    Update to work with 2 leds, buttons

--- testaus/oneLed.py
+++ testaus/oneLed.py
@@ -15,32 +15,38 @@ delay = 0.1#Tähän ei kannata koskea, jos laittaa esim 1, ei nappi välttämät
 
 #Näitä toki voi muuttaa, mutta en menis mielellään koskemaan. Menee sekavaks nopeasti.
     #Tulevaisuuden kannalta pikemminki "inPinOne, outPinOne, inPinTwo, outPinTwo jne"
-inpin = 40
-outpin = 38
-#PlaceHolder
-#PlaceHolder
-#PlaceHolder
-#PlaceHolder
-#PlaceHolder
-#PlaceHolder
+inpin1 = 40
+outpin1 = 38
+inpin2 = 37
+outpin2 = 35
+#PlaceHolder3
+#PlaceHolder3
+#PlaceHolder4
+#PlaceHolder4
 
 GPIO.setmode(GPIO.BOARD)#Ei mitää hajua tästä. Tuskin tarvii monistaa
 
 
-GPIO.setup(outpin, GPIO.OUT)
+GPIO.setup(outpin1, GPIO.OUT)
+GPIO.setup(outpin2, GPIO.OUT)
+#PlaceHolder3
+#PlaceHolder4
 #tän voi laittaa PUD_DOWNiksi, mutta se ei oikeen toimi halutulla tavalla. Vaatii vissiin myös, että vaihtaa try blockin sisällä logiikan päinvastaiseks
-GPIO.setup(inpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#PlaceHolder
-#PlaceHolder
-#PlaceHolder
+GPIO.setup(inpin1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(inpin2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#PlaceHolder3
+#PlaceHolder4
 
 
-GPIO.output(outpin, GPIO.LOW)
-#PlaceHolder
-#PlaceHolder
-#PlaceHolder
+GPIO.output(outpin1, GPIO.LOW)
+GPIO.output(outpin2, GPIO.LOW)
+#PlaceHolder3
+#PlaceHolder4
 
-ledState = False
+ledState1 = False
+ledState2 = False
+#PlaceHolde3
+#PlaceHolder4
 
 
 def turnOnLed(ledPin):
@@ -49,8 +55,8 @@ def turnOnLed(ledPin):
 def turnOffLed(ledPin):
     GPIO.output(ledPin, GPIO.LOW)
     
-def toggleLed(ledPin):
-    global ledState
+def toggleLed(ledPin, ledState):
+    #global ledState
     ledState = not ledState
     
     if ledState:
@@ -58,17 +64,19 @@ def toggleLed(ledPin):
     else:
         turnOffLed(ledPin)
     
+    return ledState
+    
 '''
 #YLEMPI TRY BLOKKI turnOnLedille ja turnOffLedille
 try:
     while True:
-        #turnOffLed(outpin)
-        readVal = GPIO.input(inpin)
+        #turnOffLed(outpin1)
+        readVal = GPIO.input(inpin1)
         print(readVal)
         if readVal == 0:
-            turnOnLed(outpin)
+            turnOnLed(outpin1)
         else:
-            turnOffLed(outpin)
+            turnOffLed(outpin1)
     
         sleep(delay)
         
@@ -80,23 +88,24 @@ except KeyboardInterrupt:
 #ALEMPI TRY BLOKK toggleLedille
 try:
     while True:
-        readVal = GPIO.input(inpin)
+        readVal1 = GPIO.input(inpin1)
+        readVal2 = GPIO.input(inpin2)
+        #PlaceHolder
+        #PlaceHolder
+        
+        print("readVal1 is:", readVal1, "readVal2 is:", readVal2)#Tässä bugi. Printtaa aina 1 paitsi jos painat, mut heti sen jälkeen taas 1
         #PlaceHolder
         #PlaceHolder
         #PlaceHolder
         
-        print("Value of readVal is", readVal)#Tässä bugi. Printtaa aina 1 paitsi jos painat, mut heti sen jälkeen taas 1
-        #PlaceHolder
-        #PlaceHolder
-        #PlaceHolder
-        
-        if readVal == 0:
-            toggleLed(outpin)
-            
-            #Tää fixaa välkkymisen, jos nappi on pohjassa
-            while GPIO.input(inpin) == 0:
-                pass
-        #PlaceHolder
+        if readVal1 == 0:
+            ledState1 = toggleLed(outpin1, ledState1)
+            sleep(delay)#ettei paina monesti
+
+        if readVal2 == 0:
+            ledState2 = toggleLed(outpin2, ledState2)
+            sleep(delay)#ettei paina monesti
+
         #PlaceHolder
         #PlaceHolder
             
@@ -106,3 +115,4 @@ try:
 except KeyboardInterrupt:#tää käynnistyy painamalla Ctrl + C
     GPIO.cleanup()
     print("GPIO cleaned up")
+
